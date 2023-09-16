df21927b293e3b11c45eda4a833411c61c2281ba
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-09-16 15:09:15 +0530

    Combine array using spread operator

---
+++ ES6/Concept5.js
@@ -0,0 +1,9 @@
+//Spread Operator (... used different than rest parameter)
+
+let ar=[1,2,3,4,5];
+let arr=[6,7,8,9,10];
+let combine=[...ar,...arr];
+console.log(combine); // [ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10 ]
+console.log(ar); // [ 1, 2, 3, 4, 5 ]
+console.log(arr); // [ 6, 7, 8, 9, 10 ]
+
