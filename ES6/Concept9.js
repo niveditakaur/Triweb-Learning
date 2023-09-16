798d754ea8ff019f441cd14e417b7a1fcfc29c67
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-09-16 19:15:53 +0530

    Object Matching , Deep Matching

---
+++ ES6/Concept9.js
@@ -0,0 +1,18 @@
+//  , Deep Matching
+
+function get(){
+    let obj={
+        ename: "Raj",
+        salary: 1000,
+        location: {
+            city:"Bangalore"
+        }
+    }
+    return obj;
+}
+
+let{ename,salary}= get();
+console.log("Name:", ename, "Salary:", salary); // Object Matching
+
+let{ename: employeename,salary: employeesalary, location: {city: employeecity}} = get(); //using object
+console.log("Name:", employeename, "Salary:", employeesalary, "City", employeecity); // Deep Matching
