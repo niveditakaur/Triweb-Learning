571f9533b405290bb0d4beebf727d99f51b18809
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-09-16 15:30:53 +0530

    Template Literal `

---
+++ ES6/Concept6.js
@@ -0,0 +1,10 @@
+// Template Literals
+
+function display(sname, age)
+{
+    let prints= `My name is ${sname} and I am ${age} years old` ;
+    console.log(prints);
+}
+let sname= "Nivedita";
+let age = 20;
+display(sname, age);
