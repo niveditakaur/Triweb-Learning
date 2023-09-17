51c575e97429f38c542f63cb43f9d65577759774
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-09-17 14:55:06 +0530

    Map Object Key

---
+++ ES6/Concept13.js
@@ -0,0 +1,11 @@
+//Map Object Key
+
+const student1={sname: "Nivedita"};
+const student2={sname: "Akshita"};
+
+const studentMap = new Map();
+
+studentMap.set(student1, "Good student");
+studentMap.set(student2, "Avg student");
+
+console.log(studentMap);
