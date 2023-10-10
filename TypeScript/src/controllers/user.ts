1bdddc9f2a0ba180c2ac71fb2c24e6a55134be8b
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-10 22:46:27 +0530

    type interface, union of type, readonly property

--- TypeScript/src/controllers/user.ts
+++ TypeScript/src/controllers/user.ts
@@ -1,15 +1,42 @@
 import { Request,Response } from 'express';
 
-import {addUserToDB} from '../models/user';
+import {addUserToDB,updateUserData} from '../models/user';
 
 const registerUser = (req: Request, res: Response)=>{
 
-    const userName = "Nivedita";
-    const password = "password";
+    const userName: string = "Nivedita";
+    const password: string = "password";
+    const age = 20;
 
-    const result = addUserToDB(userName,password);
+    const result = addUserToDB(userName,password,age);
 
     res.send(result);
 }
 
-export {registerUser}
+const checkTypesAndInterface = (req:Request,res:Response)=>{
+
+    interface Point{
+        readonly x:number,
+        readonly y:number 
+    }
+    let num:Point = { x:1, y:2};
+    //num.x=4; //Cannot assign to 'x' because it is a read-only property.
+    
+    type lockedState = "locked" | "unlocked"; //union type self created type 
+    let xState: lockedState = "locked";
+
+
+
+    const myUser = {
+        id:1,
+        uname:"Nivedita",
+        password:"psw123",
+        age:20,
+        is_active: false,
+        accountStatus: "locked"
+    }
+    const result = updateUserData(myUser);
+    res.send(result);
+}
+
+export {registerUser,checkTypesAndInterface}
--- TypeScript/src/routes/user.ts
+++ TypeScript/src/routes/user.ts
@@ -1,8 +1,9 @@
 const express = require('express');
 //const userController = require('../controllers/user');
-import {registerUser} from '../controllers/user';
+import {registerUser, checkTypesAndInterface} from '../controllers/user';
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/update', checkTypesAndInterface);
 
 export default router;
