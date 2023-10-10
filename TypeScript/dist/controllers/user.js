6fe230117fd124e5a10a9e5ef98481e7e91efbbe
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-10 22:49:09 +0530

    Converted source code to TypeScript

--- TypeScript/dist/controllers/user.js
+++ TypeScript/dist/controllers/user.js
@@ -1,11 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerUser = void 0;
+exports.checkTypesAndInterface = exports.registerUser = void 0;
 const user_1 = require("../models/user");
 const registerUser = (req, res) => {
     const userName = "Nivedita";
     const password = "password";
-    const result = (0, user_1.addUserToDB)(userName, password);
+    const age = 20;
+    const result = (0, user_1.addUserToDB)(userName, password, age);
     res.send(result);
 };
 exports.registerUser = registerUser;
+const checkTypesAndInterface = (req, res) => {
+    let num = { x: 1, y: 2 };
+    num.x = 4;
+    let xState = "locked";
+    const myUser = {
+        id: 1,
+        uname: "Nivedita",
+        password: "psw123",
+        age: 20,
+        is_active: false,
+        accountStatus: "locked"
+    };
+    const result = (0, user_1.updateUserData)(myUser);
+    res.send(result);
+};
+exports.checkTypesAndInterface = checkTypesAndInterface;
