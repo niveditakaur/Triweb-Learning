e730961ba4ec06bbaefd5d0947c7aef499dde578
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-16 23:38:26 +0530

    Add Quiz Routes and Authentication Middleware

---
+++ Project/QuizManager/src/routes/quiz.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+import {createQuiz,getQuiz,updateQuiz,deleteQuiz,publishQuiz} from '../controllers/quiz';
+import {isAuthenticated} from '../middlewares/isAuth';
+
+const router = express.Router();
+
+router.post("/",isAuthenticated,createQuiz);
+
+router.get("/:quizId",isAuthenticated,getQuiz);
+
+router.put("/",isAuthenticated,updateQuiz);
+
+router.delete("/:quizId",isAuthenticated,deleteQuiz);
+
+router.patch("/publish",isAuthenticated,publishQuiz);
+
+
+export default router;
