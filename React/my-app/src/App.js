73cdccb1c98cece4a92d0b185ce8c9aaaa402ee8
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-22 23:36:43 +0530

    Imported MainMenu

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,10 +2,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
+import MainMenu from "./components/layouts/MainMenu";
 
 function App() {
   return (
     <div>
+      <MainMenu/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
