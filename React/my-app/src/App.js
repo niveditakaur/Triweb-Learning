c4fd339f8bdf985c68c4acb8931a527afd774a72
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-23 00:10:33 +0530

    Imported Product Page

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Product from "./pages/Product";
 import { Routes, Route } from "react-router-dom";
 import MainMenu from "./components/layouts/MainMenu";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/product" element={<Product />}></Route>
       </Routes>
     </div>
   );
