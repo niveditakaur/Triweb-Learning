d901a0c4ba5b1d140621da3d88a371fffba6b36d
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-23 17:20:39 +0530

    Added Product Component with ProductList Rendering

--- React/my-app/src/pages/Product.js
+++ React/my-app/src/pages/Product.js
@@ -1,28 +1,34 @@
+import ProductList from "../components/product/ProductList";
 function Product(){
 
     let products = [
         {
             id:'p1',
             product_name:"City Tour",
-            image:"",
+            image:"https://drive.google.com/uc?export=download&id=1ME2pkXO1XM1vyuSoPF-qSDw660Sx_5rX",
             description:"It is a good tour",
             price:"20000"
         },
         {
             id:'p2',
             product_name:"Forest Tour",
-            image:"forest.jpg",
+            image:"https://drive.google.com/uc?export=download&id=1pAz0wbSvHtvbQ1pM9lFHeZmcoDsFgvc-",
             description:"It is a thrilling tour",
             price:"50000"
+        },
+        {
+            id:'p3',
+            product_name:"River Side Tour",
+            image:"https://drive.google.com/uc?export=download&id=1CsREOz0cljoKlt5tGxZvBRp8lbcVu97a",
+            description:"It is a refreshing tour",
+            price:"40000"
         }
     ]
 
     return <div>
-        <ul>
-        {products.map((item)=>{
-            return <li key={item.id}>{item.product_name}</li>;
-        })}            
-        </ul>
+              
+        <ProductList products={products}/>   
+
     </div>
 
 }
