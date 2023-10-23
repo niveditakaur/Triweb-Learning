dba63151cddee40ba596c73430fcce177f70f754
Nivedita Kaur <niveditakaur5244@gmail.com>
2023-10-23 20:48:13 +0530

    Implemented Express server with MongoDB connection and 'POST /product' route for adding products. Added error handling and status messages for product creation.

---
+++ React/my-app-backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const Product = require('./model/product');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.connectionString || "";
+
+app.get('/', (req, res) => {
+    res.send("It is runnning ");
+});
+
+app.post('/product', async (req, res)=>{
+    try {
+        const result = await Product.create(req.body);
+        console.log(result);
+        res.send({status: "success", message: "Product added"});
+    } catch (error) {
+        console.log(error.message);
+        res.send({status:'error',message:error.message});
+    }
+})
+
+
+mongoose.connect(uri)
+    .then(() => {
+        app.listen(process.env.PORT, () =>
+            console.log("Server is running"));
+    })
+    .catch(error => console.log(error))
